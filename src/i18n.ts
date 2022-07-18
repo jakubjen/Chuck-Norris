a5e748f3a2e24843184cf590b1ace49d7aa0dea5
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-07-18 14:01:42 +0200

    add Typescript

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
-const resources = {
+export const resources = {
   en: {
     translation: {
       'Welcome to React': 'Welcome to React and react-i18next',
