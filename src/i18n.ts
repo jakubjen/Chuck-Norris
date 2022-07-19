7bc0a2c3926a3324ce3f8e06e6e4db94279813a4
Jakub Jen <poczta.jakub.jen@gmail.com>
2022-07-19 11:38:55 +0200

    Move translation to json file

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,23 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import translationEn from '../public/locales/en.json';
 
-// the translations
-// (tip move them in a JSON file and import them,
-// or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 export const resources = {
   en: {
-    translation: {
-      'Welcome to React': 'Welcome to React and react-i18next',
-      SaveJoke_one: 'Save joke',
-      SaveJoke_other: 'Save jokes',
-      JokeCounterRangeError: 'You can pick a number from 1 to 100.',
-      DownloadError: 'Something goes wrong. Try again later',
-      DrawRandomJoke: 'Draw a radom {{name}} joke.',
-      ImpersonateChuckNorris: 'Impersonate Chuck Norris',
-      Categories: 'Categories',
-      HttpError: 'Chuck is on vacation. Try again later',
-      loading: 'Loading',
-    },
+    translation: translationEn,
   },
 };
 
